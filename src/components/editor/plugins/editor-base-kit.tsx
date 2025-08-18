ccd0c0e567b6f24c70cd6b018990cc4a5b63ebe3
lsherman98 <levisherman98@gmail.com>
2025-08-18 13:18:56 -0400

    remove unused files

--- src/components/editor/plugins/editor-base-kit.tsx
+++ src/components/editor/plugins/editor-base-kit.tsx
@@ -2,14 +2,12 @@ import { BaseAlignKit } from "./align-base-kit";
 import { BaseBasicBlocksKit } from "./basic-blocks-base-kit";
 import { BaseBasicMarksKit } from "./basic-marks-base-kit";
 import { BaseLinkKit } from "./link-base-kit";
-import { BaseListKit } from "./list-base-kit";
 import { MarkdownKit } from "./markdown-kit";
 
 export const BaseEditorKit = [
   ...BaseBasicBlocksKit,
   ...BaseLinkKit,
   ...BaseBasicMarksKit,
-  ...BaseListKit,
   ...BaseAlignKit,
   ...MarkdownKit,
 ];
